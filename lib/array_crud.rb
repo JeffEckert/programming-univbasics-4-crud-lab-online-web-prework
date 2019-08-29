8542874f895745828cf0f7ed70f7e94948b3a046
Jeffery Eckert <bromodo@gmail.com>
2019-08-29 23:43:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,6 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  famous_cats = ["lil' bub", "grumpy cat", "Maru","mufasa"]
+  
   
 end
 
